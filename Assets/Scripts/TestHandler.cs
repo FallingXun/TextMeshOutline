1cf916b956c5c86e0607e13b22621a315245be2a
Xun <276819846@qq.com>
2020-11-25 13:52:57 +0800

    新增投影测试效果

--- Assets/Scripts/TestHandler.cs
+++ Assets/Scripts/TestHandler.cs
@@ -69,6 +69,9 @@ public class TestHandler : MonoBehaviour
             float v = Random.Range(0f, 1f);
             tests[i].text.faceDilate = u;
             tests[i].text.outlineWidth = v;
+            tests[i].text.underlayOffsetX = 0.3f;
+            tests[i].text.underlayOffsetY = 0.3f;
+            tests[i].text.underlayDilate = u;
             tests[i].text.effectColorFloat = new Vector4(u, v, u, 1f);
         }
     }
